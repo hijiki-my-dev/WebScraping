aab9ab4a3c38be3177caea9c1ca477b8f3dde7c5
Hijiki <hijikiblog01@gmail.com>
2023-03-19 21:08:32 +0900

    コメントを修正しただけ

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 def title_only(s):
     #sはBeautifulSoup()で作る引数
     #電撃文庫の今月と来月発売の作品タイトルとURLを抜粋。
-    #タイトルと発売日を順番に表示するにはループを使って1つずつselect_oneする？
+    #タイトルと発売日を順番に表示するにはループを使う。elmsは配列だから、それで回す。
     elms = s.select(".p-books-media__title > a")
     print(elms[0])
 
