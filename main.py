330077d2ea0ecb062d5bb27d97b37f8300947ed2
Hijiki <hijikiblog01@gmail.com>
2023-03-20 10:37:43 +0900

    .gitignoreを追加して、ファイルを分割

--- main.py
+++ main.py
@@ -2,33 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+import module
 
-def title_only(s):
-    #sはBeautifulSoup()で作る引数
-    #電撃文庫の今月と来月発売の作品タイトルとURLを抜粋。
-    #タイトルと発売日を順番に表示するにはループを使う。elmsは配列だから、それで回す。
-    elms = s.select(".p-books-media__title > a")
-    print(elms[0])
 
-def main():
-    url = "https://dengekibunko.jp/product/newrelease-bunko.html"
-    
-    time.sleep(1)
-    r = requests.get(url)
-    
-    soup = BeautifulSoup(r.content, "html.parser")
-    
-    title_only(soup)
-    
-    
-    
-    #elms = soup.find_all("a")
-    #print(soup.select("h1"))
-    
-    #print(r.headers)
-    #print(r.content)
-    
-    
-    
-if __name__ == "__main__":
-    main()
+url = "https://dengekibunko.jp/product/newrelease-bunko.html"
+
+time.sleep(1)
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, "html.parser")
+
+module.title_only(soup)
+
+
+
+#elms = soup.find_all("a")
+#print(soup.select("h1"))
+
+#print(r.headers)
+#print(r.content)
