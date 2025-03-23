2403f6bd5e31602465c856d4b238d005184697e6
Hijiki <hijikiblog01@gmail.com>
2025-03-23 18:01:15 +0900

    fix: cloud storage process

--- src/modules/storage.py
+++ src/modules/storage.py
@@ -2,18 +2,21 @@ import ast
 
 from google.cloud import storage
 
+from utils import Logger, log_level
+
+logger = Logger(log_level)
+
 class StorageClient:
     def __init__(self, bucket_name):
         self.client = storage.Client()
+        logger.debug(f"client: {self.client}")
         self.bucket = self.client.bucket(bucket_name)
+        logger.debug(f"bucket: {self.bucket}")
 
     def get_reading_book_list(self, file_name):
         blob = self.bucket.blob(file_name)
-        content = blob.download_as_string()
+        content = blob.download_as_text()
+        # 改行で分割してリスト化
+        words_list = [line.strip() for line in content.splitlines() if line.strip()]
 
-        # バイト列を文字列に変換
-        if isinstance(content, bytes):
-            content = content.decode('utf-8')
-        reading_book_list = ast.literal_eval(content)
-
-        return reading_book_list
+        return words_list
